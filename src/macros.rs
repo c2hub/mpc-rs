c8064d0e64e58c81ceac35d1116a1dbd42ed7a18
Lukáš Hozda <luk.hozda@gmail.com>
2017-05-27 16:24:36 +0200

    add more sweet macros

--- src/macros.rs
+++ src/macros.rs
@@ -1,34 +1,57 @@
 #[macro_export]
+macro_rules! c_str
+{
+	($str:expr) => 
+	{{
+		use std::ffi::CString;
+		CString::new($str).unwrap().as_ptr()
+	}}
+}
+
+#[macro_export]
+macro_rules! str_c
+{
+	($ptr:expr) =>
+	{{
+		use std::ffi::CStr;
+		CStr::from_ptr($ptr).to_string_lossy().into_owned()
+	}}
+}
+
+#[macro_export]
+macro_rules! dfs
+{
+	($ptr:expr, $mem:ident) =>
+	{{
+		str_c!((*$ptr).$mem)
+	}}
+}
+
+#[macro_export]
 macro_rules! parser
 {
 	($grammar:expr, $input:expr, $filename:expr, $top:ident, $($p:ident)+) =>
 	{{ unsafe {
-		use std::ffi::CString;
+		use glue;
 		use std::os::raw::c_void;
-		let $top = mpc_new(CString::new(stringify!($top)).unwrap().as_ptr());
+		let $top = mpc_new(c_str!(stringify!($top)));
 		$
 		(
-			let $p = mpc_new(CString::new(stringify!($p)).unwrap().as_ptr());
+			let $p = mpc_new(c_str!(stringify!($p)));
 		)+
 
 		mpca_lang(
 			mpca_lang_type::MPCA_LANG_DEFAULT, 
-			CString::new($grammar).unwrap().as_ptr()
+			c_str!($grammar)
 			$(, $p)+, 
 			$top, 
 			0 as *mut c_void
 		);
 
-		let res = glue_parse(
-			CString::new($filename).unwrap().as_ptr(),
-			CString::new($input).unwrap().as_ptr(),
+		glue::parse(
+			c_str!($filename),
+			c_str!($input),
 			$top,
-		);
-
-		match res.res
-		{
-			res_t::ok => Ok(res.ok),
-			res_t::err => Err(res.err)
-		}
+		)
 	}}}
 }
