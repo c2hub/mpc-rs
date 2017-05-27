14f9761ac2bac0550099e1faa34dd856cbc72d68
Lukáš Hozda <luk.hozda@gmail.com>
2017-05-27 16:24:07 +0200

    improve glue

--- src/glue.rs
+++ src/glue.rs
@@ -6,6 +6,7 @@ use mpc_c_types::*;
 use std::os::raw::c_char;
 
 #[repr(C)]
+#[derive(Eq, PartialEq)]
 pub enum res_t
 {
 	ok,
@@ -23,4 +24,15 @@ pub struct parse_result
 extern
 {
 	pub fn glue_parse(filename: *const c_char, string: *const c_char, p: *mut mpc_parser_t) -> parse_result;
+}
+
+pub fn parse(filename: *const c_char, string: *const c_char, p: *mut mpc_parser_t) -> Result<*mut mpc_ast_t, *mut mpc_err_t>
+{
+	let res = unsafe { glue_parse(filename, string, p) };
+
+	match res.res
+	{
+		res_t::ok => Ok(res.ok),
+		res_t::err => Err(res.err),
+	}
 }
