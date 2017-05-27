d2976b00a43712306727a73718903b068c631993
Lukáš Hozda <luk.hozda@gmail.com>
2017-05-27 22:45:00 +0200

    add make_ast and ast_print tests, convert to g_string-ism

--- src/test.rs
+++ src/test.rs
@@ -1,4 +1,5 @@
 use glue;
+use ast::*;
 use mpc_c::*;
 use mpc_c_types::*;
 
@@ -8,12 +9,12 @@ fn mpca_parse()
 	let result = parser!
 	{
 		grammar:
-		{
-			"word : /[a-zA-Z0-9]+/;                         \n".to_string() +
-			"punct: '.' | '!' | ',' | ';' | '?' | '-' | ':';\n" +
-			"sentence: <word>+ <punct>;                     \n" +
+		{g_string![
+			"word : /[a-zA-Z0-9]+/;                         \n"
+			"punct: '.' | '!' | ',' | ';' | '?' | '-' | ':';\n"
+			"sentence: <word>+ <punct>;                     \n"
 			"paragraph: <sentence>+;                        \n"
-		}
+		]}
 		filename: {"test.txt"}
 		input:
 		{
@@ -30,6 +31,83 @@ fn mpca_parse()
 		{
 			println!("success!");
 			unsafe { mpc_ast_print(ast); }
+		},
+		Err(r) =>
+		{
+			println!("fail!");
+			unsafe { mpc_err_print(r); }
+			panic!();
+		}
+	}
+}
+
+#[test]
+fn make_ast()
+{
+	let result = parser!
+	{
+		grammar:
+		{g_string![
+			"word : /[a-zA-Z0-9]+/;                         \n"
+			"punct: '.' | '!' | ',' | ';' | '?' | '-' | ':';\n"
+			"sentence: <word>+ <punct>;                     \n"
+			"paragraph: <sentence>+;                        \n"
+		]}
+		filename: {"test.txt"}
+		input:
+		{
+			"A big brown piece of shit jumped over something.".to_string() +
+			"Bananas are awesome. What do you mean, potato?"
+		}
+		main: paragraph
+		parsers: word punct sentence
+	};
+
+	match result
+	{
+		Ok(a) =>
+		{
+			let ast: Ast = Ast::new(a);
+			traceln!("len:" ast.children.len());
+			traceln!("tag:" ast.tag);
+		},
+		Err(r) =>
+		{
+			println!("fail!");
+			unsafe { mpc_err_print(r); }
+			panic!();
+		}
+	}
+}
+
+#[test]
+fn print_ast()
+{
+	let result = parser!
+	{
+		grammar:
+		{g_string![
+			"word : /[a-zA-Z0-9]+/;                         \n"
+			"punct: '.' | '!' | ',' | ';' | '?' | '-' | ':';\n"
+			"sentence: <word>+ <punct>;                     \n"
+			"paragraph: <sentence>+;                        \n"
+		]}
+		filename: {"test.txt"}
+		input:
+		{
+			"A big brown piece of shit jumped over something.".to_string() +
+			"Bananas are awesome. What do you mean, potato?"
+		}
+		main: paragraph
+		parsers: word punct sentence
+	};
+
+	match result
+	{
+		Ok(a) =>
+		{
+			let ast: Ast = Ast::new(a);
+			ast.print();
 		},
 		Err(r) =>
 		{
